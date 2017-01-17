b5a91b9b44f9921f75be7dfcb612ec0723fb525b
Matthew Planchant <matthew.planchant@digital.hmrc.gov.uk>
2017-01-17 12:19:26 +0000

    First commit.

---
+++ src/main/java/partridge-kata-1-java/App.java
@@ -0,0 +1,13 @@
+package partridge-kata-1-java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
